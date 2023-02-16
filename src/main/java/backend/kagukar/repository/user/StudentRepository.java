bfa6cda8b2736dda2db498322ce170e269e5c5c4
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2023-02-16 04:02:53 +0100

    added updateStudents methods

--- src/main/java/backend/kagukar/repository/user/StudentRepository.java
+++ src/main/java/backend/kagukar/repository/user/StudentRepository.java
@@ -4,8 +4,14 @@ import backend.kagukar.data.user.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 
+    default  void updateStudent(Student student){
+        student.setModifiedDate(LocalDateTime.now());
+        save(student);
+    }
 
 }
