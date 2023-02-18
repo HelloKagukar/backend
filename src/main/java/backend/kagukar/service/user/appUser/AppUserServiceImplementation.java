922806d972b97f37da4c79214f97a1f643c9ad92
Joshuacodeat10 <joshuaagboola99@gmail.com>
2023-02-18 10:31:44 -0800

    refactor

--- src/main/java/backend/kagukar/service/user/appUser/AppUserServiceImplementation.java
+++ src/main/java/backend/kagukar/service/user/appUser/AppUserServiceImplementation.java
@@ -1,9 +1,13 @@
 package backend.kagukar.service.user.appUser;
 
 import backend.kagukar.data.user.AppUser;
+import backend.kagukar.data.user.Type;
 import backend.kagukar.dto.request.RegisterUserDto;
 import backend.kagukar.exception.PasswordException;
 import backend.kagukar.repository.user.AppUserRepository;
+import backend.kagukar.repository.user.parent.ParentRepository;
+import backend.kagukar.repository.user.student.StudentRepository;
+import backend.kagukar.repository.user.teacher.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,16 @@ public class AppUserServiceImplementation implements AppUserService{
 
     @Autowired
     private AppUserRepository appUserRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Autowired
+    private ParentRepository parentRepository;
+
 
     @Override
     public AppUser registerUser(RegisterUserDto user) throws PasswordException {
