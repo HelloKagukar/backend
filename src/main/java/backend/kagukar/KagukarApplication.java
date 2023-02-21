f49dd519c6c055fee5eb17e6c07e12d8d0acb5fd
OkoroaforKelechi <106816493+okoroaforkelechidivine@users.noreply.github.com>
2023-02-21 12:15:35 +0100

    fixings

--- src/main/java/backend/kagukar/KagukarApplication.java
+++ src/main/java/backend/kagukar/KagukarApplication.java
@@ -4,16 +4,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class KagukarApplication {
     public static void main(String[] args) {
         SpringApplication.run(KagukarApplication.class, args);
         System.out.println("Starting application with localization");
     }
 
+
     @Bean
-    public void modelMapper() {
-        modelMapper = new ModelMapper();
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
